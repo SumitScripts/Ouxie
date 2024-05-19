e382e87f79dddd954ebce83c4c1030ba307ede3e
iiprixe? <167163979+SkireScripts@users.noreply.github.com>
2024-05-19 06:57:38 -0400

    Create snippet code interpreter.lua

---
+++ Projects/snippet code interpreter.lua
@@ -0,0 +1,33 @@
+--[[snippets code interpreter
+	before you go after me, it runs snippets not full fucking code, so its useless
+	also couldnt find a better free limitless api
+]]
+local request = http_request or request or (syn and syn.request) or (fluxus and fluxus.request) or (http and http.request)
+local exec = {}
+function exec.run(config)
+	local run_code = request({
+		Url = "https://glot.io/run/"..language.."?version="..config.version;
+		Method = "POST";
+		Headers = game:GetService("HttpService"):JSONEncode({
+			["content-type"] = "application/json"
+		});
+		Body = game:GetService("HttpService"):JSONEncode({
+			["files"] = [
+				{
+					["name"] = "penis",
+					["content"] = game:GetService("HttpService"):JSONEncode(config.source)
+				}
+			],
+			["stdin"] = "",
+			["command"] = config.command
+		})
+	})
+	local response = game:GetService("HttpService"):JSONDecode(run_code.Body)
+	if response["stdout"] ~= nil then
+		return game:GetService("HttpService"):JSONDecode(response.stdout)
+	else
+		warn(run_code.Body)
+		return ""
+	end
+end
+return exec
